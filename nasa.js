ea286394b16870a64fd2c835ff19679e486c2515
kawboyer <kawboyer@gmail.com>
2018-03-13 12:07:55 -0400

    lunchtime push

---
+++ nasa.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDn7AW0NT2Rd5mvGgf4WVeYT8ov4aVd2IA",
+authDomain: "nasatube-6866e.firebaseapp.com",
+databaseURL: "https://nasatube-6866e.firebaseio.com",
+projectId: "nasatube-6866e",
+storageBucket: "nasatube-6866e.appspot.com",
+messagingSenderId: "1031253820980"
+};
+firebase.initializeApp(config);
+
