1ab6280bfe09dd28c3600ee52c5c1141f3447cd9
kawboyer <kawboyer@gmail.com>
2018-03-13 23:58:56 -0400

    added tweet button and messed around with layout

--- nasa.js
+++ nasa.js
@@ -10,3 +10,36 @@ messagingSenderId: "1031253820980"
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+
+// Click event for the search button ("#seach-button")
+// Click event for entering "Enter" key to start search ("#search-enter")
+// Reloads a new page and a search within 
+
+$("#search.button").on("click", function() {
+    event.preventDefault();
+    var userSearch = $("#userSearch")
+
+});
+
+
+
+// Nasa API return description to populate the id ("#description-box)
+
+// Click event Thumbs-up increasing voting rate for specific image ("#thumbs-up")
+
+// AJAX call for Nasa API
+
+// 
+
+
+
+// AJAX call for the Youtube API
+
+// Media queries need to be implemented
+
+
+
+
+
